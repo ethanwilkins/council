b44d0527d6980c37210c10efe644f4f5f64482b2
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-04-05 17:38:34 -0400

    testing

--- client/src/containers/WebFinger.js
+++ client/src/containers/WebFinger.js
@@ -5,8 +5,11 @@ import axios from 'axios';
 export class WebFinger extends Component {
 
   callWebFinger = async () => {
+    console.log("Got here 1");
     const resource = queryString.parse(this.props.location.search)['resource'];
+    console.log("Got here 2");
     const response = await axios.get('/.well-known/webfinger', { params: { resource: resource } });
+    console.log("Got here 3");
     return await JSON.stringify(response.data);
   };
 
@@ -21,14 +24,15 @@ export class WebFinger extends Component {
     this.callWebFinger()
       if (!this.state.data) {
         this.callWebFinger().then(data => this.setState({data}))
-          .catch(err => { console.log(err) });
+          .catch(err => { console.log("Could not load WebFinger.") });
+          console.log("Got here 4");
       }
   };
 
   render() {
     return (
       <div>
-        { this.state.data ?  this.state.data : null}
+        { this.state.data ?  this.state.data : "Loading..."}
       </div>
     );
   }

830447a51f03bc4c827aedee2e3d300a9251e349
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-04-14 14:11:23 -0400

    await1111

--- backend/routes/actorRoute.js
+++ backend/routes/actorRoute.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = new express.Router();
 const crypto = require('crypto');
 const request = require('request');
-const axios = require('axios');
 
 const User = require('../models/userModel');
 

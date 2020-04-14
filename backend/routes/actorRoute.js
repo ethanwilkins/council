35cde0e590cd921cc385dc048121ba9c7c22deb3
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-04-14 12:24:27 -0400

    followers111

--- backend/routes/actorRoute.js
+++ backend/routes/actorRoute.js
@@ -6,10 +6,10 @@ const User = require('../models/userModel');
 
 router.get('/:name/followers', async (req, res) => {
   const { name } = req.params;
-  // const domain = req.headers.host;
+  const domain = req.headers.host;
   // const domain = req.headers.origin.replace('https://', '');
   // const domain = req.app.get('domain');
-  const domain = req.hostname;
+  // const domain = req.hostname;
   try {
     const user = await User.findOne({ name: name }).exec();
     if (!user) {
